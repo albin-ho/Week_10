18ac57ebfbe4f496edb8666b9ecffe60e65ceaf0
Albin Ho <albin_ho@yahoo.co.uk>
2025-04-14 14:06:56 +0100

    Started Program

---
+++ ex1.cpp
@@ -0,0 +1,37 @@
+// Program Purpose:Exercise 1, Template Function that takes 2 numeric value to
+// see if they are equal Author: Last Modified:14/04/2025
+//
+//**********************************************************************************
+// #include <cmath>
+// #include <cstdlib>
+#include <iostream>
+// #include <string>
+// #include <vector>
+// #include <iomanip>
+// #include <ctime>
+// #include <fstream>
+// #include <iostream>
+using namespace std;
+// Define function Template
+template <class numero>
+// Define function
+void check_if_equal(numero &a, numero &b) {
+  if (a == b) {
+    cout << "Equal" << endl;
+  } else {
+    cout << "Not Equal" << endl;
+  }
+}
+int main() {
+  int x, y;
+  float p, q;
+
+  cout << "Type two integer numbers: ";
+  cin >> x >> y;
+  cout << "The typed integers are: " << "\n";
+  check_if_equal(x, y);
+  cout << "Type two float numbers: ";
+  cin >> p >> q;
+  cout << "The typed float are: " << "\n";
+  check_if_equal(p, q);
+}
